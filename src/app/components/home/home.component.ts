026d5f5366191db521796455f535cd3cc59be0aa
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2022-02-25 15:00:04 +0100

    WIP

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/models/IProduct';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  product: IProduct[] = [];
+
+  constructor(private service: ProductsService) { }
 
   ngOnInit(): void {
+    this.service.getProduct();
   }
 
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IProduct } from '../models/IProduct';
+import { IProductData } from '../models/IProductData';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  private productData = new Subject<IProduct[]>();
+  productData$ = this.productData.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  // Hämta data från API utifrån struktur på interfacet
+  getProduct() {
+    this.http
+    .get<IProductData>(environment.productURL)
+    .subscribe((dataFromProductApi) => {
+      this.productData.next(dataFromProductApi.products);
+      console.log(dataFromProductApi.products);
+    })
+  }
+
 }
